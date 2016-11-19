3a3cfa35d1c4a94c2522cc4ecf2b952fdcd5d261
John Doe <john@sample.com>
2016-11-19 23:18:03 +0200

    Some comments and cleanup

--- features/step_defenition/hooks.js
+++ features/step_defenition/hooks.js
@@ -2,5 +2,6 @@ module.exports = function () {
   this.After(function() {
     return this.driver.quit();
   });
+  // This works and I have no idea why I should read the documentation :)
   this.setDefaultTimeout(60 * 1000);
 };
